207ba53bdf998edbe4c5aaf68550ddfd6a28de9a
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-01-05 11:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function FizBuzz(){
+	for(let i=0;i<20;i++){
+		if (i%3==0) {
+			alert("Fizz");
+		}
+		else if(i%5==0){
+			alert("Buzz")
+		}
+		else if(i%3==0 && i%5==0){
+			alert("FizzBuzz")
+		}
+		else{
+			alert(i);
+		}
+		
+	}
+}
